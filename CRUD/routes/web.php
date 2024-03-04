d19b3b9a2fdbde7171a822bc687f218ab33eaf6c
laporka <kns198006@gmail.com>
2024-03-04 19:37:00 +0200

    Created CRUD laravel

---
+++ CRUD/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TableController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/table', [TableController::class, 'table']);
